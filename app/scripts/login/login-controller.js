42b6f56e092d0ec6e34b88480841c30a2d7522bc
Sai Gudigundla <saip106@msn.com>
2014-10-08 12:56:21 -0400

    removed state from controller

--- app/scripts/login/login-controller.js
+++ app/scripts/login/login-controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('ngTestingApp')
-        .controller('LoginController', function ($state) {
+        .controller('LoginController', function () {
             var self = this;
 
             self.credentials = {
